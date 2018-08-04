4c7f1df305f7d68f60fb8fb36cea4259f8489b47
yuntaeil <zkrxpdlf0808@gmail.com>
2018-08-04 18:19:05 +0900

    renamed

---
+++ app/src/main/java/com/tqeil/highthon_ice_bee/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.tqeil.highthon_ice_bee
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun name() : Int{
+
+        return 0;
+    }
+}
